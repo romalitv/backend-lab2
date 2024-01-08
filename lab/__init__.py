db356317ac6bcd4c07de0d7d0c160f84e587c931
romalitv <youlikeyoupass@gmail.com>
2024-01-08 21:15:00 +0200

    added config

--- lab/__init__.py
+++ lab/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config.from_pyfile('config.py', silent=True)
+
 from lab.views import user
 from lab.views import category
 from lab.views import record
